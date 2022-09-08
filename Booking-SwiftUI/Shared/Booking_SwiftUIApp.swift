324c5e2f9e33782ded67b747914a099b14495e85
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-08 23:29:26 +0500

    ✨

---
+++ Booking-SwiftUI/Shared/Booking_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Booking_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Safar Safarov on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Booking_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
