ff960a28d3f28ce0f92bcbc037a32dfec201968f
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-10 16:35:37 +0500

    ✨

--- Booking-SwiftUI/Booking-SwiftUI/ContentView.swift
+++ Booking-SwiftUI/Booking-SwiftUI/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
                 
                 CustomTabBar(currentTab: $currentTab)
             }
-//            .ignoresSafeArea(.keyboard)
+            .ignoresSafeArea(.keyboard)
         }
     }
 }
